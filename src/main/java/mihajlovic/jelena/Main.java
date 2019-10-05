f8422affe45487e14ff8e9012a18416a5e5fa7a7
jelenam86 <jelena.mat86@gmail.com>
2019-10-05 22:12:29 +0200

    Refactor one method

--- src/main/java/mihajlovic/jelena/Main.java
+++ src/main/java/mihajlovic/jelena/Main.java
@@ -8,7 +8,8 @@ import org.json.JSONObject;
 public class Main {
 
 	public static void main(String[] args) throws JSONException, IOException {
-		JSONObject berry = PokeAPI.findTheLargestBerry(PokeAPI.getBerries(), "growth_time", "size");
+		JSONObject berry = PokeAPI.findTheLargestBerryYouCanGrowInTheShortestTime
+				(PokeAPI.getBerries(), "growth_time", "size");
 		System.out.println(berry.getString("name") + " is the largest berry you can grow in the shortest time. URL: "
 				+ PokeAPI.findBerries().get(berry.getInt("id") - 1));
 	}
